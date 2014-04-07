eff594adafdb96db553e60515eb1168c37958ef8
Anton Kjörck Lindén <anton@noviumdesign.se>
2014-04-07 14:23:17 +0200

    Initial from template

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,5 +1,5 @@
-//= require jquery/jquery
 //= require foundation/js/foundation.min
 
 //= require app
+//= require jquery.fullPage.min.js
 //= require script
---
+++ source/javascripts/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $.fn.fullpage({
+        scrollingSpeed: 700,
+        easing: 'linear',
+        css3: false,
+        anchors:['optimera', 'konvertera', 'garantera', 'referera', 'kaffe-med-mera'],
+        verticalCentered: false,
+        scrollOverflow: true
+    });
+});
